39981c4aa63c38592ff2b28d9cea4ee120129abc
Anderson Pereira da Silva <andersonp.silva25@gmail.com>
2023-05-08 18:31:46 -0300

    :sparkles: Feat: Implementing the process function in the file_process module.

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,18 @@
+from ting_file_management.file_management import txt_importer as file
+import sys
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    file_data = {
+        "nome_do_arquivo": path_file,
+        "qtd_linhas": len(file(path_file)),
+        "linhas_do_arquivo": file(path_file),
+    }
+    for index in range(len(instance)):
+        if instance.search(index)["nome_do_arquivo"] == path_file:
+            return None
+
+    instance.enqueue(file_data)
+    print(file_data, file=sys.stdout)
 
 
 def remove(instance):

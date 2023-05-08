3c0ad91422f7bb33bb797fb72c42352f103790d9
Anderson Pereira da Silva <andersonp.silva25@gmail.com>
2023-05-08 16:58:17 -0300

    :sparkles: Feat: Implement a txt_importer function, to import news from a file

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,11 @@
+import sys
+
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+    try:
+        if path_file.endswith(".txt"):
+            with open(path_file, "r") as file:
+                return [line.strip("\n") for line in file.readlines()]
+        print("Formato inválido", file=sys.stderr)
+    except FileNotFoundError:
+            print(f"Arquivo {path_file} não encontrado", file=sys.stderr)

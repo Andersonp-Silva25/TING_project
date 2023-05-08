a41856a76b5d2cc4ebf9919173ab8bb58e3a3260
Anderson Pereira da Silva <andersonp.silva25@gmail.com>
2023-05-08 16:32:15 -0300

    :sparkles: Feat: Implementing a queue to store the files that will be read

--- ting_file_management/queue.py
+++ ting_file_management/queue.py
@@ -3,16 +3,22 @@ from ting_file_management.abstract_queue import AbstractQueue
 
 class Queue(AbstractQueue):
     def __init__(self):
-        """Inicialize sua estrutura aqui."""
+        self._queue = list()
 
     def __len__(self):
-        """Aqui irá sua implementação"""
+        return len(self._queue)
+
+    def is_empty(self):
+        return self._queue == []
 
     def enqueue(self, value):
-        """Aqui irá sua implementação"""
+        self._queue.append(value)
 
     def dequeue(self):
-        """Aqui irá sua implementação"""
+        if not self.is_empty():
+            return self._queue.pop(0)
 
     def search(self, index):
-        """Aqui irá sua implementação"""
+        if index in range(len(self._queue)):
+            return self._queue[index]
+        raise IndexError("Índice Inválido ou Inexistente")

7c04c9b92f4fb850735f03ecf77fa00dc67e41c3
Anderson Pereira da Silva <andersonp.silva25@gmail.com>
2023-05-09 17:10:51 -0300

    :sparkles: Feat: Implementing the exists_word function, inside the word_search module, to check if the word exists in the file

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,5 +1,25 @@
+import re
+
+
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    for index in range(len(instance)):
+        file = instance.search(index)
+        line = file["linhas_do_arquivo"]
+        file_name = file["nome_do_arquivo"]
+
+        data = {
+            "palavra": word,
+            "arquivo": file_name,
+            "ocorrencias": [],
+        }
+        
+        for i in range(len(line)):
+            if re.search(word, line[i], re.I):
+                data["ocorrencias"].append({"linha": i + 1})
+
+        result = [data] if len(data["ocorrencias"]) != 0 else []
+
+    return result
 
 
 def search_by_word(word, instance):
